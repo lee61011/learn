49cef406a2aa0eb9a3bbcab949458b4035e660c7
lee61011 <lee61011@126.com>
2019-07-14 23:04:05 +0800

    Text Widget 文本组件的使用

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -15,7 +15,26 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter')
         ),
         body: Center(
-          child: Text('Hello World 123'),
+          child: Text(
+            'TextAlign属性就是文本的对齐方式，它的属性值有如下几个',
+            //  textAlign: TextAlign.center | TextAlign.left | TextAlign.right | TextAlign.start | TextAlign.end
+            textAlign: TextAlign.left,
+
+            //  maxLines: 设置最多显示的行数
+            maxLines: 1,
+
+            //  overflow: 设置文本溢出o
+            //  overflow: TextOverflow.ellipsis | TextOverflow.clip | TextOverflow.fade
+            overflow: TextOverflow.ellipsis,
+
+            //  style:  设置样式  参考：https://docs.flutter.io/flutter/painting/TextStyle-class.html
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color.fromARGB(255, 255, 150, 150),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.dashed,
+            ),
+          ),
         ),
       ),
     );
