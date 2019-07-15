3a94d54f54f03a6f32bb694bcfa8941ad75750c4
lee61011 <lee61011@126.com>
2019-07-15 16:58:41 +0800

    Container 容器组件的使用（padding margin decoration）

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -20,11 +20,26 @@ class MyApp extends StatelessWidget {
             
             //  Alignment 属性是针对 Container 内 child 的对齐方式，也就是容器字内容的对齐方式，并不是容器本身的对齐方式
             //  Alignment.center | centerLeft | centerRight | bottomCenter | bottomLeft | bottomRight | topLeft | topCenter | topRight
-            alignment: Alignment.center,
+            alignment: Alignment.topLeft,
 
             width: 500.0,
             height: 400.0,
-            color: Colors.lightBlue,
+            // color: Colors.lightBlue,
+
+            // padding: EdgeInsets.all(10.0),
+            padding: EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
+
+            // margin: EdgeInsets.all(10.0),
+            margin: EdgeInsets.fromLTRB(10.0, 20.0, 5.0, 0.0),
+
+            //  decoration 是 container 的修饰器，主要功能是设置背景和边框
+            decoration: new BoxDecoration(
+              //  这里在给 container 设置渐变色的时候，需要注释掉上面的 color，否则会起冲突
+              gradient: const LinearGradient(
+                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
+              ),
+              border: Border.all(width: 2.0, color: Colors.red)
+            ),
           ),
         ),
       ),
