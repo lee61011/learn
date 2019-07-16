0c8a668dad7af2073b493341aad4bd603e5ee2cb
lee61011 <lee61011@126.com>
2019-07-16 23:56:12 +0800

    ListView 列表组件

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -12,32 +12,28 @@ class MyApp extends StatelessWidget {
       title: 'Welcome Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter')
+          title: Text('ListView Widget')
         ),
-        body: Center(
-          child: Container(
-            //  image.asset(资源图片) | network(网络资源图片) | file(本地图片) | memory(加载 Unit8List 资源图片)
-            child: new Image.network(
-              'http://blogimages.jspang.com/blogtouxiang1.jpg',
-              // 'https://p3.ssl.qhimgs1.com/bdr/326__/t01f446ba25b5af3183.jpg',
-              // scale: 1.0,
-
-              //  fit: 根据父级容器控制图片的拉伸和挤压
-              //  fit: BoxFit.fill | contain | cover | fitWidth | fitHeight | scaleDown
-              fit: BoxFit.contain,
-
-              //  图片的混合模式： color 是要混合的颜色，colorBlendMode 是混合模式
-              color: Colors.greenAccent,
-              colorBlendMode: BlendMode.darken,
-
-              //  repeat 图片重复
-              //  ImageRepeat.repeat | repeatX | repeatY
-              repeat: ImageRepeat.repeat,
+        body: new ListView(
+          children: <Widget>[
+            new ListTile(
+              leading: new Icon(Icons.access_time),
+              title: new Text('access_time'),
             ),
-            width: 300.0,
-            height: 200.0,
-            color: Colors.lightBlue,
-          ),
+            new ListTile(
+              leading: new Icon(Icons.access_alarm),
+              title: new Text('access_alarm'),
+            ),
+            new ListTile(
+              leading: new Icon(Icons.access_alarms),
+              title: new Text('access_alarms'),
+            ),
+
+            new Image.network('http://jspang.com/static/upload/20181111/G-wj-ZQuocWlYOHM6MT2Hbh5.jpg'),
+            new Image.network('http://jspang.com/static/upload/20181109/1bHNoNGpZjyriCNcvqdKo3s6.jpg'),
+            new Image.network('http://jspang.com/static/upload/20181111/G-wj-ZQuocWlYOHM6MT2Hbh5.jpg'),
+            new Image.network('http://jspang.com/static/upload/20181109/1bHNoNGpZjyriCNcvqdKo3s6.jpg'),
+          ],
         ),
       ),
     );
