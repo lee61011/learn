f77db15837632fe2736f4a87b203aafdab1cdc72
lee61011 <lee61011@126.com>
2019-07-17 23:56:59 +0800

    横向列表的使用

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -14,29 +14,63 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('ListView Widget')
         ),
-        body: new ListView(
-          children: <Widget>[
-            new ListTile(
-              leading: new Icon(Icons.access_time),
-              title: new Text('access_time'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.access_alarm),
-              title: new Text('access_alarm'),
-            ),
-            new ListTile(
-              leading: new Icon(Icons.access_alarms),
-              title: new Text('access_alarms'),
-            ),
+        body: Center(
+          child: Container(
+            height: 200.0,
+            /* child: new ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                new Container(
+                  width: 180.0,
+                  color: Colors.lightBlue,
+                ),
+                new Container(
+                  width: 180.0,
+                  color: Colors.amber,
+                ),
+                new Container(
+                  width: 180.0,
+                  color: Colors.deepOrange,
+                ),
+                new Container(
+                  width: 180.0,
+                  color: Colors.deepPurpleAccent,
+                )
+              ],
+            ), */
 
-            new Image.network('http://jspang.com/static/upload/20181111/G-wj-ZQuocWlYOHM6MT2Hbh5.jpg'),
-            new Image.network('http://jspang.com/static/upload/20181109/1bHNoNGpZjyriCNcvqdKo3s6.jpg'),
-            new Image.network('http://jspang.com/static/upload/20181111/G-wj-ZQuocWlYOHM6MT2Hbh5.jpg'),
-            new Image.network('http://jspang.com/static/upload/20181109/1bHNoNGpZjyriCNcvqdKo3s6.jpg'),
-          ],
+            child: MyList(),
+          ),
         ),
       ),
     );
   }
 }
 
+
+class MyList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: <Widget>[
+        new Container(
+          width: 180.0,
+          color: Colors.lightBlue,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.amber,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.deepOrange,
+        ),
+        new Container(
+          width: 180.0,
+          color: Colors.deepPurpleAccent,
+        )
+      ],
+    );
+  }
+}
