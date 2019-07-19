b183913bdcb1d959201ea24f23d3a87d87c0e19e
lee61011 <lee61011@126.com>
2019-07-19 23:16:32 +0800

    水平布局 Row 的使用

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -12,59 +12,30 @@ class MyApp extends StatelessWidget {
       title: 'Welcome Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('ListView Widget')
+          title: Text('水平方向布局')
         ),
-        body: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            
-             // crossAxisSpacing: 网格间的空挡  | crossAxisCount: 网格的列数
-            crossAxisCount: 3,
-            mainAxisSpacing: 2.0,
-            crossAxisSpacing: 2.0,
-            childAspectRatio: 0.7
-          ),
-
+        body: new Row(
           children: <Widget>[
-            new Image.network('http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',fit: BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg',fit: BoxFit.cover),
+            new RaisedButton(
+              onPressed: () {},
+              color: Colors.redAccent,
+              child: new Text('红色按钮')
+            ),
+
+            //  在按钮外面加上 Expanded 可以占据剩余宽度，类似于弹性盒子
+            Expanded(child: new RaisedButton(
+              onPressed: (){},
+              color: Colors.orangeAccent,
+              child: new Text('橙色按钮'),
+            ),),
+
+            new RaisedButton(
+              onPressed: (){},
+              color: Colors.pinkAccent,
+              child: new Text('粉色按钮'),
+            )
           ],
         )
-      ),
-    );
-  }
-}
-
-
-class MyList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        new Container(
-          width: 180.0,
-          color: Colors.lightBlue,
-        ),
-        new Container(
-          width: 180.0,
-          color: Colors.amber,
-        ),
-        new Container(
-          width: 180.0,
-          color: Colors.deepOrange,
-        ),
-        new Container(
-          width: 180.0,
-          color: Colors.deepPurpleAccent,
-        )
-      ],
     );
   }
 }
