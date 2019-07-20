c0018228e42a7aea685533729d6fa5142b229ca8
lee61011 <lee61011@126.com>
2019-07-20 22:20:33 +0800

    垂直布局 Column 组件

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('垂直方向布局')
         ),
-        body: Column(
+        /* body: Column(
 
           //  主轴和副轴
           //    main轴：如果你用 column 组件，那垂直就是主轴；如果你用 row 组件，那么水平就是主轴
@@ -27,17 +27,19 @@ class MyApp extends StatelessWidget {
             Text("what's your problem?"),
             Text("what's your name?"),
           ],
-        )
+        ) */
 
         //  页面居中显示
-        /* body: Column(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Center(child: Text('are you ok?')),
             Center(child: Text("what's your problem?")),
+            // Expanded 包起来的占据页面剩余距离
+            // Expanded(child: Center(child: Text("what's your problem?"))),
             Center(child: Text("what's your name?"))
           ],
-        ) */
+        )
       )
     );
   }
