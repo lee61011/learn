960c863f522448e227a724b39546c09557f9fa29
lee61011 <lee61011@126.com>
2019-07-24 19:24:23 +0800

    一般页面导航和返回

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -2,45 +2,47 @@
 import 'package:flutter/material.dart';
 
 //  入口
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: '导航演示1',
+    home: new FirstScreen(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    var card = new Card(
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: new Text('吉林省吉林市昌邑区',style: TextStyle(fontWeight: FontWeight.w500),),
-            subtitle: new Text('技术胖:123456789'),
-            leading: new Icon(Icons.account_box, color: Colors.lightBlue,),
-          ),
-          new Divider(),
-          ListTile(
-            title: new Text('北京市海淀区中国科技大学', style: TextStyle(fontWeight: FontWeight.w500),),
-            subtitle: new Text('技术胖:123456789'),
-            leading: new Icon(Icons.account_box, color: Colors.lightBlue,),
-          ),
-          new Divider(),
-          ListTile(
-            title: new Text('河南省濮阳市百姓办公楼', style: TextStyle(fontWeight: FontWeight.w500),),
-            subtitle: new Text('技术胖:123456789'),
-            leading: new Icon(Icons.account_box, color: Colors.lightBlue,),
-          ),
-          new Divider(),  
-        ],
+    return new Scaffold(
+      appBar: AppBar(title: Text('导航页面'),),
+      body: Center(
+        child: RaisedButton(
+          child: Text('查看商品详情页面'),
+          onPressed: (){
+            //  Navigator.push 是跳转到下一个页面
+            Navigator.push(context, new MaterialPageRoute(
+              builder: (context) => new SecondScreen()
+            ));
+          }
+        ),
       ),
     );
+  }
+}
 
-    return MaterialApp(
-      title: 'Welcome Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('层叠布局')
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('商品详情页面'),),
+      body: Center(child: RaisedButton(
+        child: RaisedButton(
+          child: Text('返回'),
+          onPressed: (){
+            //  Navigator.pop 是返回到上一个页面
+            Navigator.pop(context);
+          },
         ),
-        body: Center(child: card),
-      )
+      ),),
     );
   }
 }
