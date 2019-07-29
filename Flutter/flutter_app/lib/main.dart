ac2da75010a54ac5dc10eae1386925e31de07ee0
lee61011 <lee61011@126.com>
2019-07-29 17:14:36 +0800

    静态资源和项目图片的处理

--- Flutter/flutter_app/lib/main.dart
+++ Flutter/flutter_app/lib/main.dart
@@ -1,71 +1,13 @@
 //  引入样式库
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(MaterialApp(
-    title: '页面跳转返回数据',
-    home: FirstPage(),
-  ));
-}
+void main()=>runApp(MyApp());
 
-class FirstPage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('找小姐姐要电话'),),
-      body: Center(
-        child: RouteButton(),
-      ),
-    );
-  }
-}
-
-//  跳转的 Button
-class RouteButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: (){
-        _navigateToXiaoJieJie(context);
-      },
-      child: Text('去找小姐姐'),
-    );
-  }
-
-  _navigateToXiaoJieJie(BuildContext context) async {
-    //  async 是启用异步方法
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context)=> XiaoJieJie())
-    );
-
-    Scaffold.of(context).showSnackBar(SnackBar(content: Text('$result'),));
-  }
-}
-
-class XiaoJieJie extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('我是小姐姐'),),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('大长腿小姐姐'),
-              onPressed: (){
-                Navigator.pop(context, '大长腿：110');
-              },
-            ),
-            RaisedButton(
-              child: Text('小蛮腰小姐姐'),
-              onPressed: (){
-                Navigator.pop(context, '小蛮腰：110');
-              },
-            ),
-          ],
-        ),
-      ),
+  Widget build(BuildContext context){
+    return Container(
+      child: Image.asset('images/touxiang.jpg'),
     );
   }
 }
