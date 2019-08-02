c4aa4a2abef972a28cede5a0aefbd1e915404d36
lee61011 <lee61011@126.com>
2019-08-02 22:50:32 +0800

    ReactDemo

---
+++ React/ReactDemo/demo01/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render(){
+        return(
+            <div>Hello ReactJS!</div>
+        )
+    }
+}
+
+export default App;
