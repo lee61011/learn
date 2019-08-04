9a72664253b9897460e68522a9a805587ec62c91
lee61011 <lee61011@126.com>
2019-08-04 21:23:51 +0800

    响应式设计和数据的绑定

--- React/ReactDemo/demo01/src/Books.js
+++ React/ReactDemo/demo01/src/Books.js
@@ -1,18 +1,34 @@
 import React, {Component, Fragment} from 'react'
 
 class Books extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+
     render(){
         return(
             //  React 要求必须在一个组件的最外层进行包裹
             //  但是有时候我们的布局就偏不需要这层包裹，我们可以引入 Fragment 标签来替换 div 标签，在浏览器的 Elements 中查看，最外层已经没有包裹了
             <Fragment>
-                <div><input /><button>增加书籍</button></div>
+                <div><input value={this.state.inputValue} onChange={this.inputChange.bind(this)} /><button>增加书籍</button></div>
                 <ul>
                     <li>JavaScript 高级程序设计第三版</li>
                     <li>Vue.js 实战</li>
                 </ul>
             </Fragment>
         )
+    }
+
+    inputChange(e) {
+        // console.log(e.target.value)
+        this.setState({
+            inputValue: e.target.value
+        })
     }
 }
 
