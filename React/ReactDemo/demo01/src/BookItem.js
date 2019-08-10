2d90676f81556adfb3b93019725ede957f715760
lee61011 <lee61011@126.com>
2019-08-10 19:43:35 +0800

    PropTypes 校验传递值

--- React/ReactDemo/demo01/src/BookItem.js
+++ React/ReactDemo/demo01/src/BookItem.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class BookItem extends Component {
-    state = {  }
+    state = {}
     render() { 
         return ( 
-            <div onClick={this.handleClick.bind(this)}>{this.props.content}</div>
+            <li onClick={this.handleClick.bind(this)}>
+                {this.props.content}
+            </li>
          );
     }
 
@@ -12,6 +15,12 @@ class BookItem extends Component {
         //  console.log(this.props.index)
         this.props.deleteItem(this.props.index)
     }
+}
+
+BookItem.propTypes = {
+    content: PropTypes.string,
+    deleteItem: PropTypes.func,
+    index: PropTypes.number.isRequired
 }
  
 export default BookItem;
