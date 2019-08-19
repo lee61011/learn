0f630a6ed30cedc96d7b1fad7dcea02879f6f56a
lee61011 <lee61011@126.com>
2019-08-19 23:50:18 +0800

    创建 Redux 中的仓库 store 和 reducer

--- React/ReduxDemo/demo01/src/TodoList.js
+++ React/ReduxDemo/demo01/src/TodoList.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 
-const data = [
+import store from './store'
+
+/* const data = [
     '早8点开晨会，分配今天的开发工作',
     '早9点和项目经理作开发需求讨论会',
     '晚5:30对今日代码进行review'
-]
+] */
 
 class TodoList extends Component {
-    state = {  }
+    constructor(props){
+        super(props)
+        this.state = store.getState();
+        console.log(this.state)
+    }
     render() { 
         return ( 
             <div style={{margin: '10px'}}>
                 <div>
-                    <Input placeholder="write someting" style={{width: '250px', marginRight: '10px'}} />
+                    <Input placeholder={this.state.inputValue} style={{width: '250px', marginRight: '10px'}} />
                     <Button type="primary">添加</Button>
                 </div>
                 <div style={{margin: '10px', width: '300px'}}>
                     <List 
                         bordered
-                        dataSource={data}
+                        dataSource={this.state.list}
                         renderItem={item=>(<List.Item>{item}</List.Item>)}
                     />
                 </div>
