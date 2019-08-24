c2f21f000a57c61d252645057822ea9988344f1e
lee61011 <lee61011@126.com>
2019-08-24 22:42:06 +0800

    组件UI和业务逻辑的拆分

--- React/ReduxDemo/demo01/src/TodoList.js
+++ React/ReduxDemo/demo01/src/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
-import { Input, Button, List } from 'antd'
+// import { Input, Button, List } from 'antd'
+import TodoListUI from './TodoListUI'
 
 import store from './store'
 import { changeInputAction, addItemAction, deleteItemAction } from './store/actionCreators'
@@ -17,32 +18,21 @@ class TodoList extends Component {
         this.state = store.getState();
         this.changeInputValue = this.changeInputValue.bind(this)
         this.clickBtn = this.clickBtn.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
 
         this.storeChange = this.storeChange.bind(this)
         store.subscribe(this.storeChange)
     }
     render() { 
-        return ( 
-            <div style={{margin: '10px'}}>
-                <div>
-                    <Input 
-                        placeholder={this.state.inputValue} 
-                        style={{width: '250px', marginRight: '10px'}}
-                        onChange={this.changeInputValue}
-                        value={this.state.inputValue} />
-                    <Button 
-                        type="primary"
-                        onClick={this.clickBtn}>添加</Button>
-                </div>
-                <div style={{margin: '10px', width: '300px'}}>
-                    <List 
-                        bordered
-                        dataSource={this.state.list}
-                        renderItem={(item, index)=>(<List.Item onClick={this.deleteItem.bind(this,index)}>{item}</List.Item>)}
-                    />
-                </div>
-            </div>
-        );
+        return (
+            <TodoListUI
+                inputValue = {this.state.inputValue}
+                list = {this.state.list}
+                changeInputValue = {this.changeInputValue}
+                clickBtn = {this.clickBtn}
+                deleteItem = {this.deleteItem}
+            />
+        )
     }
 
     changeInputValue(e) {
