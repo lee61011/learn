78e16da113b0359aa66568dbd56d2a0707222086
lee61011 <lee61011@126.com>
2019-09-07 19:57:51 +0800

    useEffect 代替常用生命周期函数

--- React/ReactHooksDemo/demo01/src/App.js
+++ React/ReactHooksDemo/demo01/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import Example2 from './Example2'
+import Example3 from './Example3'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -6,6 +8,16 @@ function App() {
     <div>
       <p>You clicked {count} times</p>
       <button onClick={()=>{setCount(count+1)}}>click me</button>
+
+      <div>
+        <h2>useState 多状态声明</h2>
+        <Example2></Example2>
+      </div>
+
+      <div>
+        <h2>useEffect 代替常用生命周期函数</h2>
+        <Example3></Example3>
+      </div>
     </div>
   )
 }
