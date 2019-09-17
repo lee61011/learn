12c0f28159ab06d890655e6e02174f4231f18468
lee61011 <lee61011@126.com>
2019-09-17 18:49:01 +0800

    路由跳转

--- React/NextDemo/demo02/next-create/pages/index.js
+++ React/NextDemo/demo02/next-create/pages/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/* 标签式导航 */
+/* import React from 'react'
 import Link from 'next/link'
 
 const Home = () => (
@@ -8,5 +9,38 @@ const Home = () => (
     <div><Link href="/jspangB"><a>去 JspangB 页面</a></Link></div>
   </>
 )
+
+export default Home */
+
+
+/* 编程式导航 */
+import React from 'react'
+import Link from 'next/link'
+import Router from 'next/router'
+
+const Home = () => {
+  function gotoA() {
+    Router.push('/jspangA')
+  }
+  return (
+    <>
+      <div>我是首页</div>
+      <div>
+        <Link href="/jspangA">
+          <a>
+            <span>去JspangA页面</span>
+            <span>前端博客</span>
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/jspangB"><a>去JspnagB页面</a></Link>
+      </div>
+      <div>
+        <button onClick={gotoA}>去JspangA页面</button>
+      </div>
+    </>
+  )
+}
 
 export default Home
