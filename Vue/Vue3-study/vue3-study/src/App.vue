4a901e0075da4987a6754753c971ae0ca353eddc
lee61011 <lee61011@126.com>
2019-10-10 19:35:25 +0800

    Vue3.x 新特性

---
+++ Vue/Vue3-study/vue3-study/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <com-setup />
+  </div>
+</template>
+
+<script>
+  import SetupCom from './components/setup'
+
+  export default {
+    name: 'app',
+    components: {
+      "com-setup": SetupCom
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ Vue/Vue3-study/vue3-study/src/components/setup.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Setup.vue 页面</h2>
+    </div>
+</template>
+
+<script>
+import {} from "@vue/composition-api"
+
+export default {
+    setup() {
+        //  setup 函数会在 beforeCreate 之后、created 之前执行
+        console.log("setup");
+    },
+    beforeCreate() {
+        console.log("beforeCreate");
+    },
+    created() {
+        console.log("created");
+    },
+}
+</script>
