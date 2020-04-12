404e845c8fe2d10aa3f458566d9b35bc74acfe04
lee61011 <lee61011@126.com>
2020-04-12 23:56:31 +0800

    Cascader

--- Vue/vue-cascader/src/App.vue
+++ Vue/vue-cascader/src/App.vue
@@ -1,27 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cascader :potions="options"></Cascader>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Cascader from './components/Cascader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Cascader,
+  },
+  data() {
+    return {
+      options: [
+        {
+          label: '肉类',
+          children: [
+            {
+              label: '猪肉',
+              children: [
+                {
+                  label: '五花肉',
+                },
+                {
+                  label: '里脊肉',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+// #app
+//   font-family Avenir, Helvetica, Arial, sans-serif
+//   -webkit-font-smoothing antialiased
+//   -moz-osx-font-smoothing grayscale
+//   text-align center
+//   color #2c3e50
+//   margin-top 60px
 </style>
---
+++ Vue/vue-cascader/src/components/Cascader.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="cascader">
+    <div class="title" @click="toggle"></div>
+    <div class="content" v-if="isVisible">
+      显示的内容
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+  methods: {
+    close() {
+      this.isVisible = false;
+    },
+    toggle() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+};
+</script>
+
+<style lang="stylus">
+  .title
+    width 150px;
+    height 30px;
+    border 1px solid #ccc;
+</style>
