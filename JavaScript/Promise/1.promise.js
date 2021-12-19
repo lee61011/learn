ee1ad6201168ec437052a4214dc6eaf1dcc1e40b
lee61011 <lee61011@126.com>
2021-12-19 11:03:22 +0800

    Promise then链式调用实现方案

--- JavaScript/Promise/1.promise.js
+++ JavaScript/Promise/1.promise.js
@@ -10,19 +10,52 @@
 // 1. promise有三个状态：成功态(resolve) 失败态(reject) 等待态(pending)
 // promise就是一个类
 
-let Promise = require('./promise')
-let promise = new Promise((resolve, reject) => {
-  throw new Error('失败了')
-  resolve('成功')
-  reject('失败')
-})
-promise.then((data) => {
-  console.log('success', data);
+// let Promise = require('./promise')
+// let promise = new Promise((resolve, reject) => {
+//   throw new Error('失败了')
+//   resolve('成功')
+//   reject('失败')
+// })
+// promise.then((data) => {
+//   console.log('success', data);
+// }, (err) => {
+//   console.log('faild', err);
+// })
+// promise.then((data) => {
+//   console.log('success1', data);
+// }, (err) => {
+//   console.log('faild1', err);
+// })
+
+
+let fs = require('fs')
+
+function read(filename) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
+      if(err) return reject(err)
+      resolve(data)
+    })
+  })
+}
+// 1. promise 成功和失败的回调的返回值 可以传递到外层的下一个then
+// # 2. 如果返回的是普通值的话(传递到下一次的成功中)，出错的情况(一定会走下一次的失败)，可能还有promise的情况(会采用promise的状态，决定走下一次的成功还是失败)
+// 3. 错误处理 如果离自己最近的then 没有错误处理 会向下找
+read('./name.txt').then((data) => {
+  console.log(data);
+  return 100
 }, (err) => {
-  console.log('faild', err);
+  console.log(err);
+  throw err
+}).then((data) => {
+  console.log('---------- ', data);
+}, err => {
+  console.log('---------- ', err);
 })
-promise.then((data) => {
-  console.log('success1', data);
-}, (err) => {
-  console.log('faild1', err);
-})
+
+// # error first 错误第一   异步方法无法通过try、catch捕获异常
+// fs.readFile('./name.txt', 'utf8', (err, data) => {
+//   fs.readFile(data, 'utf8', (err, data) => {
+//     console.log(data);
+//   })
+// })
--- JavaScript/Promise/promise.js
+++ JavaScript/Promise/promise.js
@@ -10,6 +10,43 @@ const RESOLVED = 'RESOLVED' // 成功
 const REJECTED = 'REJECTED' // 失败
 const PENDING = 'PENDING' // 等待态
 
+// resolvePromise 所有的promise都要支持 bluebird q es6-promise
+const resolvePromise = (promise2, x, resolve, reject) => {
+  // 1. 循环引用 自己等待自己完成 错误的实现
+  if(promise2 === x) { // 用一个类型错误 结束掉promise
+    return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
+  }
+  // # 后续的条件要严格判断 保证代码能和别的库一起使用
+  let called
+  if ((typeof x === 'object' && x != null) || typeof x === 'function') { // 有可能是一个promise
+    // 要继续判断
+    try {
+      let then = x.then
+      if (typeof then === 'function') { // 只能认为是一个promise了
+        // 不要写成x.then 直接then.call就可以了 因为x.then会再次取值
+        then.call(x, y => { // 根据promise的状态决定是成功还是失败
+          if (called) return
+          called = true
+          // resolve(y)
+          resolvePromise(promise2, y, resolve, reject) // 递归解析
+        }, e => {
+          if (called) return
+          called = true
+          reject(e)
+        })
+      } else { // 对象
+        resolve(x)
+      }
+    } catch (e) { // 防止失败了再次进入成功
+      if (called) return
+      called = true
+      reject(e) // 取值出错
+    }
+  } else {
+    resolve(x)
+  }
+}
+
 class Promise {
   constructor(executor) {
     this.status = PENDING
@@ -41,21 +78,54 @@ class Promise {
 
   // 发布订阅模式 如果当前状态是pending时 我们需要将成功的回调和失败的回调存放起来 稍后调用resolve和reject的时候重新执行
   then(onFulfilled, onRejected) {
-    if (this.status === RESOLVED) {
-      onFulfilled(this.value)
-    }
-    if (this.status === REJECTED) {
-      onRejected(this.reason)
-    }
-    if (this.status === PENDING) {
-      this.onResolvedCallbacks.push(() => {
-        // todo ... 切片
-        onFulfilled(this.value)
-      })
-      this.onRejectedCallbacks.push(() => {
-        onRejected(this.reason)
-      })
-    }
+    let promise2 = new Promise((resolve, reject) => { // # 为了实现链式调用
+      if (this.status === RESOLVED) {
+        setTimeout(() => {
+          try {
+            let x =onFulfilled(this.value)
+            // resolve(x)
+            // x可能是一个promise
+            resolvePromise(promise2, x, resolve, reject) // # 这里的promise2需要等new Promise执行完成后才能拿到结果 所以需要用setTimeout包起来 否则是undefined
+          } catch (e) {
+            reject(e) // # 外层的trycatch无法捕获异步方法错误 所以这里要用reject捕获错误状态
+          }
+        }, 0);
+      }
+      if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            let x = onRejected(this.reason)
+            resolvePromise(promise2, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0)
+      }
+      if (this.status === PENDING) {
+        this.onResolvedCallbacks.push(() => {
+          // todo ... 切片
+          setTimeout(() => {
+            try {
+              let x = onFulfilled(this.value)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+        this.onRejectedCallbacks.push(() => {
+          setTimeout(() => {
+            try {
+              let x = onRejected(this.reason)
+              resolvePromise(promise2, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0);
+        })
+      }
+    })
+    return promise2
   }
 }
 
